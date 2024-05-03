2938801432df0dc60d33cc8c2e1feacbd8fdc5b1
Rudy Clark <rudy.uzb@gmail.com>
2024-05-03 09:47:28 +0500

    cmake main input arguments header file added

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include "cmakeConfig.h"
 
-int main() {
+int main(int argc, char** argv) {
 
 	std::cout << "Hello World\n";
-	std::cin.get();
+    if (argc < 2) {
+        // report ver
+        std::cout << argv[0] << " Version " << CMakeHelloWorld_VERSION_MAJOR << "."
+            << CMakeHelloWorld_VERSION_MINOR << std::endl;
+        std::cout << "Usage: " << argv[0] << " number" << std::endl;
+    }
+
+    std::cin.get();
 
 	return 0;
 }
